3f364f3ad3f16c5fa8855f14996f60ac4896166f
CarolinaGaitan <dianagaitannegocios@gmail.com>
2022-10-09 23:48:27 -0500

    commit

--- src/main/java/com/alquiler/nube/repository/crudRepository/AdminCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alquiler.nube.repository.crudRepository;
-
-import com.alquiler.nube.entities.Admin;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminCrudRepository extends CrudRepository <Admin, Integer> {
-}
